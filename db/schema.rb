7bc513458327819b38c1c7354ed56976a065a5b4
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-12 15:34:04 +0800

    rails generate: model Movie and controller Movies and set up resources for routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_12_071119) do
+  create_table "movies", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date_released"
+    t.string "country_of_origin"
+    t.datetime "showing_start"
+    t.datetime "showing_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
