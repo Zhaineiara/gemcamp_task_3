aa95cb98a9673061f53b6133d5524e3fd5ff408f
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-13 18:59:52 +0800

    Added: genres in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#Genre.destroy_all
+
+genres = [
+  { name: 'Action' },
+  { name: 'Comedy' },
+  { name: 'Drama' },
+  { name: 'Horror' },
+  { name: 'Thriller' },
+  { name: 'Science Fiction' },
+  { name: 'Romance' },
+  { name: 'Adventure' },
+  { name: 'Fantasy' },
+  { name: 'Mystery' },
+  { name: 'Documentary' },
+  { name: 'Animation' },
+  { name: 'Musical' },
+  { name: 'Western' },
+  { name: 'Historical' },
+  { name: 'Crime' },
+  { name: 'War' },
+  { name: 'Sport' },
+  { name: 'Family' },
+  { name: 'Biography' }
+]
+
+genres.each do |genre_attributes|
+  Genre.create(genre_attributes)
+end
